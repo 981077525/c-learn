7e455483ed878bb01e0393199da3bec2c39a43bd
小鹏 <74500183+981077525@users.noreply.github.com>
2020-11-22 12:54:07 +0800

    Add files via upload

---
+++ 2020/九九乘法表c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//目标：打印9*9乘法表
+int main(){
+	int a = 1;
+	int b = 1;
+	for (a = 1; a < 10; ++a)
+	{
+			for (b = 1; b <= a; ++b)
+			{
+				printf("%d * %d = %d  ", b, a,a*b);
+				
+			}
+			printf("\n");
+	}
+	return 0;
+}
